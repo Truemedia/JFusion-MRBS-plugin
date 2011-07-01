6d2bbe5df4c2712430d0320899a1350201721648
Wade Penistone <truemedia@mediacityonline.net>
2011-07-01 22:06:09 +0100

    changed function loadsetup to read db variables specifically for MRBS

--- admin.php
+++ admin.php
@@ -49,12 +49,11 @@ class JFusionAdmin_mrbs extends JFusionAdmin
     function loadSetup($storePath) {
         //check for trailing slash and generate file path
         if (substr($storePath, -1) == DS) {
-            $myfile = $storePath . 'config/settings.inc.php';
+            $myfile = $storePath . 'config.inc.php';
         } else {
-            $myfile = $storePath . DS . 'config/settings.inc.php';
+            $myfile = $storePath . DS . 'config.inc.php';
         }
         if (($file_handle = @fopen($myfile, 'r')) === false) {
-            JError::raiseWarning(500, JText::_('WIZARD_FAILURE') . ": $myfile " . JText::_('WIZARD_MANUAL'));
             $result = false;
             return $result;
         } else {
@@ -63,23 +62,26 @@ class JFusionAdmin_mrbs extends JFusionAdmin
             $file_handle = fopen($myfile, 'r');
             while (!feof($file_handle)) {
                 $line = fgets($file_handle);
-                if (strpos($line, 'define(') === 0 && count($config) <= 8) {
+                if (strpos($line, '$db') === 0 && count($config) <= 8) {
                     /* extract the name and value, it was coded to avoid the use of eval() function */
                     // name
-                    $vars_strt[0] = strpos($line, "'");
-                    $vars_end[0] = strpos($line, "',");
+                    $vars_strt[0] = strpos($line, "d");
+                    $vars_end[0] = strpos($line, " =");
                     $name = trim(substr($line, $vars_strt[0], $vars_end[0] - $vars_strt[0]), "'");     
                     // value
-                    $vars_strt[1] = strpos($line, " '");
+                    $vars_strt[1] = strpos($line, '= "');
                     $vars_strt[1]++;
-                    $vars_end[1] = strpos($line, "')");
-                    $value = trim(substr($line, $vars_strt[1], $vars_end[1] - $vars_strt[1]), "'");
-                    if($name == "_DB_TYPE_")
+                    $vars_end[1] = strpos($line, '";');
+                    $value = str_replace('"', ' ', trim(substr($line, $vars_strt[1], $vars_end[1] - $vars_strt[1]), "'"));
+                    if($name == "db_password")
                     {
-                     $value = strtolower($value);
+                    	// value
+                    	$vars_strt[1] = strpos($line, "= '");
+                    	$vars_strt[1]++;
+                    	$vars_end[1] = strpos($line, "';");
+                    	$value = str_replace("'", " ", trim(substr($line, $vars_strt[1], $vars_end[1] - $vars_strt[1]), "'"));
                     }    
                     $config[$name] = $value;
-
                 }
             }
 	        fclose($file_handle);
