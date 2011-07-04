3da84f990c59a70607282e660f41a34a45f83afc
Wade Penistone <truemedia@mediacityonline.net>
2011-07-04 12:41:00 +0100

    added confirmation layer for variables read from configuration file, that default to return a manual maintenance guide if anything is missing

--- admin.php
+++ admin.php
@@ -154,86 +154,83 @@ class JFusionAdmin_mrbs extends JFusionAdmin
         return $config;
 	}
 	
+	function AmmendConfiguration($errors){
+		// display error listing what changes need to be made to configuration manually
+		/* USE JFUSION ERROR REPORTING TO MENTION EACH MISSING ITEM return $errors; */
+	}
+
 	function setupFromPath($storePath) {
 	    $config = JFusionAdmin_mrbs::loadSetup($storePath);
         if (!empty($config)) {
         	$auth = JFusionAdmin_mrbs::getAndSetConfiguration($storePath);
+        	$false_logic = array();
+        	
         	/*********************************************************************
  			* JFusion authentification settings (EDIT THESE SETTINGS WITH CAUTION)
  			*********************************************************************/
 			// How to validate the user/password. One of "none", "config", "db", "db_ext" (this is DEFAULT that JFusion adds), "pop3", "imap", "ldap" "nis" "nw" "ext"
-			if($auth["type"] == "db_ext"){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
+			if(!array_key_exists("type", $auth) && trim($auth['type']) !== "db_ext"){
+				$false_logic['type'] = "no";
 			} 
 
-			// The server to connect to
-			if($auth['db_ext']['db_system'] == $config['dbsys']){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			}  /* Or 'mysqli', 'pgsql' */
-			if($auth['db_ext']['db_host'] == $config['db_host']){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			}
+			if(array_key_exists("db_ext", $auth)){ // if credentials exist at all
+				// The server to connect to
+				if(!array_key_exists("db_system", $auth['db_ext']) && $auth['db_ext']['db_system'] !== "mysql"/* Or 'mysqli', 'pgsql' */){
+					$false_logic['db_system'] = "no";
+				}  
+			
+				if(!array_key_exists("db_host", $auth['db_ext']) && !preg_match("/^([a-zA-Z0-9])+([.]){0,1}([a-zA-Z0-9])+([.]){0,1}([a-zA-Z0-9])*$/", $auth['db_ext']['db_host'])){
+					$false_logic['db_host'] = "no";
+				}
 
-			// The MySQL username and password to connect with
-			if($auth['db_ext']['db_username'] == $config['db_login']){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
-			if($auth['db_ext']['db_password'] == $config['db_password']){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
+				// The MySQL username and password to connect with
+				if(!array_key_exists("db_username", $auth['db_ext']) && !preg_match("/^([a-zA-Z0-9])*$/", $auth['db_ext']['db_username'])){
+					$false_logic['db_username'] = "no";
+				}
+			 
+				if(!array_key_exists("db_password", $auth['db_ext']) && !preg_match("/^([a-zA-Z0-9])*$/", $auth['db_ext']['db_password'])){
+					$false_logic['db_password'] = "no";
+				} 
 
-			// The name of the database.
-			if($auth['db_ext']['db_name'] == $config['db_database']){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
+				// The name of the database.
+				if(!array_key_exists("db_name", $auth['db_ext']) && !preg_match("/^([a-zA-Z0-9])*$/", $auth['db_ext']['db_name'])){
+					$false_logic['db_name'] = "no";
+				} 
 
-			// The table that holds the authentication data
-			if($auth['db_ext']['db_table'] == $config['db_tbl_prefix'].'users'){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
+				// The table that holds the authentication data
+				if(!array_key_exists("db_table", $auth['db_ext']) && $auth['db_ext']['db_table'] !== "mrbs_".'users'){
+					$false_logic['db_table'] = "no";
+				} 
 
-			// The names of the two columns that hold the authentication data
-			if($auth['db_ext']['column_name_username'] == 'name'){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
-			if($auth['db_ext']['column_name_password'] == 'password'){
-				$true_logic[];
-			}
-			else{
-				$false_logic[];
-			} 
+				// The names of the two columns that hold the authentication data
+				if(!array_key_exists("column_name_username", $auth['db_ext']) && $auth['db_ext']['column_name_username'] !== 'name'){
+					$false_logic['column_name_username'] = "no";
+				} 
+			
+				if(!array_key_exists("column_name_password", $auth['db_ext']) && $auth['db_ext']['column_name_password'] !== 'password'){
+					$false_logic['column_name_password'] = "no";
+				} 
 
-			// This is the format of the password entries in the table. You can specify 'md5', 'sha1', 'crypt' or 'plaintext'
-			if($auth['db_ext']['password_format'] == 'md5'){
-				$true_logic[];
+				// This is the format of the password entries in the table. You can specify 'md5', 'sha1', 'crypt' or 'plaintext'
+				if(!array_key_exists("password_format", $auth['db_ext']) && $auth['db_ext']['password_format'] !== 'md5'){
+					$false_logic['password_format'] = "no";
+				}
 			}
-			else{
-				$false_logic[];
-			} 
+		else{
+			$false_logic['type'] = "no";
+			$false_logic['db_system'] = "no";
+			$false_logic['db_host'] = "no";
+			$false_logic['db_username'] = "no";
+			$false_logic['db_password'] = "no";
+			$false_logic['db_name'] = "no";	
+			$false_logic['db_table'] = "no";
+			$false_logic['column_name_username'] = "no";
+			$false_logic['column_name_password'] = "no";
+			$false_logic['password_format'] = "no";
+			}
+			if(count($false_logic) > 0){
+				JFusionAdmin_mrbs::AmmendConfiguration($false_logic);
+			}
 			
 			/* PARAMS */
             //save the parameters into array
